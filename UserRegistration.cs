cc959f31e1cee8896c8d61768132d1fa2fac31d2
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-01 12:48:39 +0530

    enter a valid first name using lambda uc 1

---
+++ UserRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    public class UserRegistration
+    {
+        //UC1
+        string Firstname = @"[A-Z]{1}[a-z]{2}$";
+        public string FirstnameCheckusingLambda(string firstName) => Regex.IsMatch(firstName, Firstname) ? "FirstName is Valid" : "FirstName is not Valid";
+
+
+
+
+
+    }
+}

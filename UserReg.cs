b1860b682f1aa50ae384a9ef55b313b7bc54cbaa
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-01 12:50:17 +0530

    enter valid last name using lambda uc 2

---
+++ UserReg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    public class UserRegistration
+    {
+        //UC1
+        string Firstname = @"[A-Z]{1}[a-z]{2}$";
+        public string FirstnameCheckusingLambda(string firstName) => Regex.IsMatch(firstName, Firstname) ? "FirstName is Valid" : "FirstName is not Valid";
+
+
+
+        //UC2
+        static string RegexLastName = "^[A-Z]{1,}[a-z]{3,}$";
+        public string validateLastNameusingLambda(string LastName) => Regex.IsMatch(LastName, RegexLastName) ? "valid" : "Ivalid";
+
+
+    }
+}
